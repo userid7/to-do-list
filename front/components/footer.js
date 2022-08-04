7052a5b1a2c6052cc6b0964afef114911787c6ab
firdan <onpeci@gmail.com>
2022-08-04 12:55:08 +0700

    add to do list

---
+++ front/components/footer.js
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+
+export default function Footer(props) {
+  return <footer className="footer">
+    <a
+      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Powered by <b>¯\_(ツ)_/¯</b>
+      {/* <span className="logo">
+        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      </span> */}
+    </a>
+  </footer>
+}
